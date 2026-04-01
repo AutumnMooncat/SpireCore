132a5f6aed159bd9034fc2b0fc2452492b914c23
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-04-01 18:55:13 -0300

    Pull over dialogue patches and extensions from Shockah.Johnson for reference

---
+++ Features/Dialogue/DialogueExtensions.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using Nickel;
+
+namespace AutumnMooncat.Spirecore.Features.Dialogue;
+
+internal static class DialogueExt
+{
+	public static int GetMinShieldLostThisTurn(this StoryNode node)
+		=> MainModFile.GetHelper().ModData.GetModDataOrDefault<int>(node, "MinShieldLostThisTurn");
+
+	public static StoryNode SetMinShieldLostThisTurn(this StoryNode node, int value)
+	{
+		MainModFile.GetHelper().ModData.SetModData(node, "MinShieldLostThisTurn", value);
+		return node;
+	}
+
+	public static int GetShieldLostThisTurn(this StoryVars vars)
+		=> MainModFile.GetHelper().ModData.GetModDataOrDefault<int>(vars, "ShieldLostThisTurn");
+
+	public static void SetShieldLostThisTurn(this StoryVars vars, int value)
+		=> MainModFile.GetHelper().ModData.SetModData(vars, "ShieldLostThisTurn", value);
+
+	public static HashSet<int> GetLastCardIdsInDeck(this Combat combat)
+		=> MainModFile.GetHelper().ModData.ObtainModData<HashSet<int>>(combat, "LastCardIdsInDeck");
+	
+	public static bool? GetJustPlayedRecycleCard(this StoryNode node)
+		=> MainModFile.GetHelper().ModData.GetOptionalModData<bool>(node, "JustPlayedRecycleCard");
+
+	public static StoryNode SetJustPlayedRecycleCard(this StoryNode node, bool? value)
+	{
+		MainModFile.GetHelper().ModData.SetOptionalModData(node, "JustPlayedRecycleCard", value);
+		return node;
+	}
+	
+	public static bool GetJustPlayedRecycleCard(this StoryVars vars)
+		=> MainModFile.GetHelper().ModData.GetModDataOrDefault<bool>(vars, "JustPlayedRecycleCard");
+
+	public static void SetJustPlayedRecycleCard(this StoryVars vars, bool value)
+		=> MainModFile.GetHelper().ModData.SetModData(vars, "JustPlayedRecycleCard", value);
+	
+	public static bool? GetStrengthened(this StoryNode node)
+		=> MainModFile.GetHelper().ModData.GetOptionalModData<bool>(node, "Strengthened");
+
+	public static StoryNode SetStrengthened(this StoryNode node, bool? value)
+	{
+		MainModFile.GetHelper().ModData.SetOptionalModData(node, "Strengthened", value);
+		return node;
+	}
+	
+	public static bool GetStrengthened(this StoryVars vars)
+		=> MainModFile.GetHelper().ModData.GetModDataOrDefault<bool>(vars, "Strengthened");
+
+	public static void SetStrengthened(this StoryVars vars, bool value)
+		=> MainModFile.GetHelper().ModData.SetModData(vars, "Strengthened", value);
+	
+	public static bool? GetDiscounted(this StoryNode node)
+		=> MainModFile.GetHelper().ModData.GetOptionalModData<bool>(node, "Discounted");
+
+	public static StoryNode SetDiscounted(this StoryNode node, bool? value)
+	{
+		MainModFile.GetHelper().ModData.SetOptionalModData(node, "Discounted", value);
+		return node;
+	}
+	
+	public static bool GetDiscounted(this StoryVars vars)
+		=> MainModFile.GetHelper().ModData.GetModDataOrDefault<bool>(vars, "Discounted");
+
+	public static void SetDiscounted(this StoryVars vars, bool value)
+		=> MainModFile.GetHelper().ModData.SetModData(vars, "Discounted", value);
+}
---
+++ Patches/DialoguePatches.cs
@@ -0,0 +1,152 @@
+using System.Linq;
+using AutumnMooncat.Spirecore.Characters;
+using AutumnMooncat.Spirecore.Features.Dialogue;
+using HarmonyLib;
+using Nickel;
+
+namespace AutumnMooncat.Spirecore.Patches;
+
+[HarmonyPatch]
+public class DialoguePatches : IRDialogue
+{
+    public static void Register(IModHelper helper)
+    {
+        helper.Events.RegisterAfterArtifactsHook(nameof(Artifact.OnPlayerPlayCard), (Card card, State state) =>
+        {
+            if (!card.GetDataWithOverrides(state).recycle)
+                return;
+            state.storyVars.SetJustPlayedRecycleCard(true);
+        }, double.NegativeInfinity);
+    }
+
+    [HarmonyPatch(typeof(StoryVars), nameof(StoryVars.ResetAfterCombatLine))]
+    public static class StoryVarResetAfterCombat
+    {
+        public static void Postfix(StoryVars __instance)
+        {
+            MainModFile.GetHelper().ModData.RemoveModData(__instance, "JustPlayedRecycleCard");
+            MainModFile.GetHelper().ModData.RemoveModData(__instance, "Strengthened");
+            MainModFile.GetHelper().ModData.RemoveModData(__instance, "Discounted");
+        }
+    }
+
+    [HarmonyPatch(typeof(StoryVars), nameof(StoryVars.ResetAfterEndTurn))]
+    public static class StoryVarResetAfterTurn
+    {
+        public static void Postfix(StoryVars __instance)
+        {
+            MainModFile.GetHelper().ModData.RemoveModData(__instance, "ShieldLostThisTurn");
+        }
+    }
+
+    [HarmonyPatch(typeof(StoryNode), nameof(StoryNode.Filter))]
+    public static class StoryNodeFilter
+    {
+        public static void Postfix(StoryNode n, State s, ref bool __result)
+        {
+            if (!__result)
+                return;
+
+            if (s.storyVars.GetShieldLostThisTurn() < n.GetMinShieldLostThisTurn())
+            {
+                __result = false;
+                return;
+            }
+            if (n.GetJustPlayedRecycleCard() is { } justPlayedRecycleCard && s.storyVars.GetJustPlayedRecycleCard() != justPlayedRecycleCard)
+            {
+                __result = false;
+                return;
+            }
+            if (n.GetStrengthened() is { } strengthened && s.storyVars.GetStrengthened() != strengthened)
+            {
+                __result = false;
+                return;
+            }
+            if (n.GetDiscounted() is { } discounted && s.storyVars.GetDiscounted() != discounted)
+            {
+                __result = false;
+                return;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(AStatus), nameof(AStatus.Begin))]
+    public static class AStatusBegin
+    {
+        public static void Prefix(AStatus __instance, State s, out int __state)
+        {
+            __state = __instance.targetPlayer ? s.ship.Get(__instance.status) : 0;
+        }
+
+        public static void Postfix(AStatus __instance, State s, Combat c, in int __state)
+        {
+            if (!__instance.targetPlayer)
+                return;
+
+            if (__instance.status == Ironclad.Entry.MissingStatus.Status && __state > 0 && s.ship.Get(Ironclad.Entry.MissingStatus.Status) <= 0)
+                c.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID(Ironclad.ID+"ReturningFromMissing") });
+            
+            if (__instance.status == Silent.Entry.MissingStatus.Status && __state > 0 && s.ship.Get(Silent.Entry.MissingStatus.Status) <= 0)
+                c.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID(Silent.ID+"ReturningFromMissing") });
+            
+            if (__instance.status == Defect.Entry.MissingStatus.Status && __state > 0 && s.ship.Get(Defect.Entry.MissingStatus.Status) <= 0)
+                c.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID(Defect.ID+"ReturningFromMissing") });
+            
+            if (__instance.status == Watcher.Entry.MissingStatus.Status && __state > 0 && s.ship.Get(Watcher.Entry.MissingStatus.Status) <= 0)
+                c.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID(Watcher.ID+"ReturningFromMissing") });
+        }
+    }
+
+    [HarmonyPatch(typeof(Ship), nameof(Ship.NormalDamage))]
+    public static class ShipNormalDamage
+    {
+        [HarmonyPriority(Priority.First)]
+        public static void Prefix(Ship __instance, out int __state)
+        {
+            __state = __instance.Get(Status.shield) + __instance.Get(Status.tempShield);
+        }
+        
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(Ship __instance, State s, in int __state)
+        {
+            var newShields = __instance.Get(Status.shield) + __instance.Get(Status.tempShield);
+            if (newShields >= __state)
+                return;
+
+            s.storyVars.SetShieldLostThisTurn(s.storyVars.GetShieldLostThisTurn() + (__state - newShields));
+        }
+    }
+
+    [HarmonyPatch(typeof(Combat), nameof(Combat.Update))]
+    public static class CombatUpdate
+    {
+        public static void Postfix(Combat __instance, G g)
+        {
+            var currentCardsInDeck = g.state.GetAllCards().Select(card => card.uuid).ToHashSet();
+            var lastCardsInDeck = __instance.GetLastCardIdsInDeck();
+
+            foreach (var cardId in currentCardsInDeck)
+            {
+                if (lastCardsInDeck.Contains(cardId))
+                    continue;
+                if (g.state.FindCard(cardId) is not { } card)
+                    continue;
+
+                var meta = card.GetMeta();
+                if (meta.deck != Ironclad.DeckEntry.Deck && NewRunOptions.allChars.Contains(meta.deck) && card.GetDataWithOverrides(g.state).temporary)
+                    __instance.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID("NewNon"+Ironclad.ID+"NonTrashTempCard") });
+                
+                if (meta.deck != Silent.DeckEntry.Deck && NewRunOptions.allChars.Contains(meta.deck) && card.GetDataWithOverrides(g.state).temporary)
+                    __instance.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID("NewNon"+Silent.ID+"NonTrashTempCard") });
+                
+                if (meta.deck != Defect.DeckEntry.Deck && NewRunOptions.allChars.Contains(meta.deck) && card.GetDataWithOverrides(g.state).temporary)
+                    __instance.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID("NewNon"+Defect.ID+"NonTrashTempCard") });
+                
+                if (meta.deck != Watcher.DeckEntry.Deck && NewRunOptions.allChars.Contains(meta.deck) && card.GetDataWithOverrides(g.state).temporary)
+                    __instance.QueueImmediate(new ADummyAction { dialogueSelector = MainModFile.MakeID("NewNon"+Watcher.ID+"NonTrashTempCard") });
+            }
+
+            MainModFile.GetHelper().ModData.SetModData(__instance, "LastCardIdsInDeck", currentCardsInDeck);
+        }
+    }
+}
