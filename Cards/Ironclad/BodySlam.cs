a92949aeb1f4650e253cd74eff66755fcab9ea51
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-04-11 15:23:44 -0300

    Created but did not use Body Slam

---
+++ Cards/Ironclad/BodySlam.cs
@@ -0,0 +1,109 @@
+﻿using Nickel;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AutumnMooncat.Spirecore.Cards.Ironclad;
+
+[IRegisterable.Ignore]
+internal sealed class BodySlam : Card, IRCard
+{
+    public static string ID => nameof(BodySlam);
+    public static ICardEntry Entry { get; set; }
+    
+    public static void Register(IModHelper helper)
+    {
+        Entry = helper.Content.Cards.RegisterCard(ID, new CardConfiguration
+        {
+            CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+            Meta = new CardMeta
+            {
+                deck = Characters.Ironclad.DeckEntry.Deck,
+                rarity = Rarity.common,
+                upgradesTo = [Upgrade.A, Upgrade.B]
+            },
+            Name = MainModFile.Bind(["card", ID, "name"]).Localize,
+            Art = IRCard.LookUpSpr(Characters.Ironclad.CardAssetPath + ID)
+        });
+    }
+    
+    public override CardData GetData(State state)
+    {
+        CardData data = new CardData()
+        {
+            cost = upgrade == Upgrade.A ? 0 : 1
+        };
+        return data;
+    }
+    
+    public override List<CardAction> GetActions(State s, Combat c)
+    {
+        List<CardAction> actions = [];
+        switch (upgrade)
+        {
+            case Upgrade.None:
+                actions = 
+                [
+                    new AAttack()
+                    {
+                        damage = GetDmg(s, 2)
+                    },
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 2), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction,
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 2), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction
+                ];
+                break;
+            case Upgrade.A:
+                actions = 
+                [
+                    new AAttack()
+                    {
+                        damage = GetDmg(s, 2)
+                    },
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 2), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction,
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 2), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction
+                ];
+                break;
+            case Upgrade.B:
+                actions = 
+                [
+                    new AAttack()
+                    {
+                        damage = GetDmg(s, 2)
+                    },
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 1), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction,
+                    MainModFile.Kokoro().ActionCosts.MakeCostAction(
+                        MainModFile.Kokoro().ActionCosts.MakeResourceCost(MainModFile.Kokoro().ActionCosts.MakeStatusResource(Status.shield), 1), 
+                        new AAttack()
+                        {
+                            damage = GetDmg(s, 2)
+                        }).AsCardAction
+                ];
+                break;
+        }
+        return actions;
+    }
+}
