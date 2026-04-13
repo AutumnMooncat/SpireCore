d907f5716da58c2b9916fe2eb3b79a736dd8ad8c
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-04-13 15:44:27 -0300

    Add TipDel option for InfoOnlyAction

--- Actions/InfoOnlyAction.cs
+++ Actions/InfoOnlyAction.cs
@@ -4,12 +4,18 @@ namespace AutumnMooncat.SpireCore.Actions;
 
 public class InfoOnlyAction : CardAction, IMultiIconAction
 {
+    public delegate List<Tooltip> TipDel(State s, InfoOnlyAction thiz);
+    public TipDel getTips;
     public List<Tooltip> tips;
     public Icon? icon;
     public List<Records.RenderPayload> extraIcons;
     
     public override List<Tooltip> GetTooltips(State s)
     {
+        if (getTips != null)
+        {
+            return getTips(s, this);
+        }
         return tips ?? [];
     }
 
