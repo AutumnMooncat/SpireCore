e641249d90cb25838e4c542af3398af2a775c4b4
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-04-23 15:39:31 -0300

    Refactor event dialogue

--- Features/Dialogue/EventDialogue.cs
+++ Features/Dialogue/EventDialogue.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using AutumnMooncat.SpireCore.Characters;
 using Nickel;
 
 namespace AutumnMooncat.SpireCore.Features.Dialogue;
@@ -9,43 +7,32 @@ internal sealed class EventDialogue : IRDialogue
 	public static void Register(IModHelper helper)
 	{
 		var loc = IRDialogue.GetLoc(locale => MainModFile.GetFile($"i18n/dialogue-event-{locale}.json").OpenRead());
-		var ironcladNodes = MakeICNodes();
-		var ironcladHard = MakeICHard();
-		var ironcladSay = MakeICSay();
-		var silentNodes = MakeSilentNodes();
-		var silentHard = MakeSilentHard();
-		var silentSay = MakeSilentSay();
-		var defectNodes = MakeDefectNodes();
-		var defectHard = MakeDefectHard();
-		var defectSay = MakeDefectSay();
-		var watcherNodes = MakeWatcherNodes();
-		var watcherHard = MakeWatcherHard();
-		var watcherSay = MakeWatcherSay();
+		
+		var normal = MakeNormalNodes();
+		var hardcoded = MakeHardcodedNodes();
+		var sayswitch = MakeSaySwitches();
 		
 		helper.Events.OnModLoadPhaseFinished += (_, phase) =>
 		{
 			if (phase != ModLoadPhase.AfterDbInit)
 				return;
-			IRDialogue.InjectStory(Ironclad.ID, CType.Ironclad, ironcladNodes, ironcladHard, ironcladSay, NodeType.@event);
-			IRDialogue.InjectStory(Silent.ID, CType.Silent, silentNodes, silentHard, silentSay, NodeType.@event);
-			IRDialogue.InjectStory(Defect.ID, CType.Defect, defectNodes, defectHard, defectSay, NodeType.@event);
-			IRDialogue.InjectStory(Watcher.ID, CType.Watcher, watcherNodes, watcherHard, watcherSay, NodeType.@event);
+			IRDialogue.InjectStory(normal, NodeType.@event);
+			IRDialogue.InjectStory(hardcoded, NodeType.@event);
+			IRDialogue.InjectSwitches(sayswitch, NodeType.@event);
 		};
 		
 		helper.Events.OnLoadStringsForLocale += (_, e) =>
 		{
-			IRDialogue.InjectLocalizations(Ironclad.ID, CType.Ironclad, loc, ironcladNodes, ironcladHard, ironcladSay, e);
-			IRDialogue.InjectLocalizations(Silent.ID, CType.Silent, loc, silentNodes, silentHard, silentSay, e);
-			IRDialogue.InjectLocalizations(Defect.ID, CType.Defect, loc, defectNodes, defectHard, defectSay, e);
-			IRDialogue.InjectLocalizations(Watcher.ID, CType.Watcher, loc, watcherNodes, watcherHard, watcherSay, e);
+			IRDialogue.LocalizeStory(loc, normal, e);
+			IRDialogue.LocalizeStory(loc, hardcoded, e);
+			IRDialogue.LocalizeSwitches(loc, sayswitch, e);
 		};
 	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeICNodes()
+
+	public static IRDialogue.DialogueRegistry<StoryNode> MakeNormalNodes()
 	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		
-		newNodes[["Shop", "0"]] = new()
+		var reg = IRDialogue.MakeNormalRegistry();
+		reg.Register(["Shop"], new()
 		{
 			lookup = ["shopBefore"],
 			bg = nameof(BGShop),
@@ -55,9 +42,9 @@ internal sealed class EventDialogue : IRDialogue
 				new Say { who = CType.Ironclad, loopTag = Anim.Ironclad.Sword },
 				new Jump { key = "NewShop" }
 			],
-		};
+		});
 		
-		newNodes[["Shop", "1"]] = new()
+		reg.Register(["Shop"], new()
 		{
 			lookup = ["shopBefore"],
 			bg = nameof(BGShop),
@@ -68,16 +55,15 @@ internal sealed class EventDialogue : IRDialogue
 				new Say { who = CType.CAT, loopTag = Anim.Squint },
 				new Jump { key = "NewShop" }
 			],
-		};
-		
-		return newNodes;
+		});
+		return reg;
 	}
 	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeICHard()
+	public static IRDialogue.DialogueRegistry<StoryNode> MakeHardcodedNodes()
 	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
+		var reg = IRDialogue.MakeHardcodedRegistry();
 		
-		newNodes[["LoseCharacterCard_{{CharacterType}}"]] = new()
+		reg.Register([CType.Ironclad, "LoseCharacterCard_{{CharacterType}}"], new()
 		{
 			oncePerRun = true,
 			bg = nameof(BGSupernova),
@@ -85,9 +71,19 @@ internal sealed class EventDialogue : IRDialogue
 			lines = [
 				new Say { who = CType.Ironclad, loopTag = Anim.Neutral }
 			]
-		};
+		});
 		
-		newNodes[["CrystallizedFriendEvent_{{CharacterType}}"]] = new()
+		reg.Register([CType.Ironclad, "LoseCharacterCard_{{CharacterType}}"], new()
+		{
+			oncePerRun = true,
+			bg = nameof(BGSupernova),
+			allPresent = [CType.Defect],
+			lines = [
+				new Say { who = CType.Defect, loopTag = Anim.Defect.Sad }
+			]
+		});
+		
+		reg.Register([CType.Ironclad, "CrystallizedFriendEvent_{{CharacterType}}"], new()
 		{
 			oncePerRun = true,
 			bg = nameof(BGCrystalizedFriend),
@@ -96,9 +92,20 @@ internal sealed class EventDialogue : IRDialogue
 				new Wait() { secs = 1.5 },
 				new Say { who = CType.Ironclad, loopTag = Anim.Ironclad.Sword }
 			]
-		};
+		});
 		
-		newNodes[["ChoiceCardRewardOfYourColorChoice_{{CharacterType}}"]] = new()
+		reg.Register([CType.Ironclad, "CrystallizedFriendEvent_{{CharacterType}}"], new()
+		{
+			oncePerRun = true,
+			bg = nameof(BGCrystalizedFriend),
+			allPresent = [CType.Defect],
+			lines = [
+				new Wait() { secs = 1.5 },
+				new Say { who = CType.Defect, loopTag = Anim.Defect.Happy }
+			]
+		});
+		
+		reg.Register([CType.Ironclad, "ChoiceCardRewardOfYourColorChoice_{{CharacterType}}"], new()
 		{
 			oncePerRun = true,
 			bg = nameof(BGBootSequence),
@@ -107,209 +114,38 @@ internal sealed class EventDialogue : IRDialogue
 				new Say { who = CType.Ironclad, loopTag = Anim.Ironclad.Mad },
 				new Say { who = CType.CAT, loopTag = Anim.Neutral }
 			]
-		};
+		});
+		return reg;
+	}
+	
+	public static IRDialogue.DialogueRegistry<Say> MakeSaySwitches()
+	{
+		var reg = IRDialogue.MakeSaySwitchRegistry();
 		
-		return newNodes;
-	}
-
-	public static Dictionary<IReadOnlyList<string>, Say> MakeICSay()
-	{
-		var newSay = new Dictionary<IReadOnlyList<string>, Say>();
-		newSay[["GrandmaShop"]] = new()
+		reg.Register(["GrandmaShop"], new()
 		{
 			who = CType.Ironclad,
 			loopTag = Anim.Neutral
-		};
-		newSay[["LoseCharacterCard"]] = new()
+		});
+		
+		
+		reg.Register(["LoseCharacterCard"], new()
 		{
 			who = CType.Ironclad,
 			loopTag = Anim.Neutral
-		};
-		newSay[["CrystallizedFriendEvent"]] = new()
+		});
+		
+		reg.Register(["CrystallizedFriendEvent"], new()
 		{
 			who = CType.Ironclad,
 			loopTag = Anim.Neutral
-		};
-		newSay[["ShopKeepBattleInsult"]] = new()
+		});
+		
+		reg.Register(["ShopKeepBattleInsult"], new()
 		{
 			who = CType.Ironclad,
 			loopTag = Anim.Neutral
-		};
-		return newSay;
+		});
+		return reg;
 	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeSilentNodes()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeSilentHard()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, Say> MakeSilentSay()
-	{
-		var newSay = new Dictionary<IReadOnlyList<string>, Say>();
-		return newSay;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeDefectNodes()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeDefectHard()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, Say> MakeDefectSay()
-	{
-		var newSay = new Dictionary<IReadOnlyList<string>, Say>();
-		return newSay;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeWatcherNodes()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, StoryNode> MakeWatcherHard()
-	{
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		return newNodes;
-	}
-	
-	public static Dictionary<IReadOnlyList<string>, Say> MakeWatcherSay()
-	{
-		var newSay = new Dictionary<IReadOnlyList<string>, Say>();
-		return newSay;
-	}
-	
-	/*public EventDialogue() : base(locale => ModEntry.Instance.Package.PackageRoot.GetRelativeFile($"i18n/dialogue-event-{locale}.json").OpenRead())
-	{
-		var johnsonDeck = ModEntry.Instance.JohnsonDeck.CType;
-		var johnsonType = ModEntry.Instance.JohnsonCharacter.CharacterType;
-		var newNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		var newHardcodedNodes = new Dictionary<IReadOnlyList<string>, StoryNode>();
-		var saySwitchNodes = new Dictionary<IReadOnlyList<string>, Say>();
-
-		ModEntry.Instance.Helper.Events.OnModLoadPhaseFinished += (_, phase) =>
-		{
-			if (phase != ModLoadPhase.AfterDbInit)
-				return;
-			InjectStory(newNodes, newHardcodedNodes, saySwitchNodes, NodeType.@event);
-		};
-		ModEntry.Instance.Helper.Events.OnLoadStringsForLocale += (_, e) => InjectLocalizations(newNodes, newHardcodedNodes, saySwitchNodes, e);
-
-		newNodes[["Shop", "0"]] = new()
-		{
-			lookup = ["shopBefore"],
-			bg = typeof(BGShop).Name,
-			allPresent = [johnsonType],
-			lines = [
-				new Say { who = johnsonType, loopTag = "flashing" },
-				new Say { who = "nerd", loopTag = "neutral", flipped = true },
-				new Jump() { key = "NewShop" }
-			],
-		};
-		newNodes[["Shop", "1"]] = new()
-		{
-			lookup = ["shopBefore"],
-			bg = typeof(BGShop).Name,
-			allPresent = [johnsonType],
-			lines = [
-				new Say { who = johnsonType, loopTag = "neutral" },
-				new Say { who = "nerd", loopTag = "neutral", flipped = true },
-				new Jump() { key = "NewShop" }
-			],
-		};
-
-		newHardcodedNodes[["LoseCharacterCard_{{CharacterType}}"]] = new()
-		{
-			oncePerRun = true,
-			bg = typeof(BGSupernova).Name,
-			allPresent = [johnsonType],
-			lines = [
-				new Say { who = johnsonType, loopTag = "neutral" },
-			],
-		};
-		newHardcodedNodes[["CrystallizedFriendEvent_{{CharacterType}}"]] = new()
-		{
-			oncePerRun = true,
-			bg = typeof(BGCrystalizedFriend).Name,
-			allPresent = [johnsonType],
-			lines = [
-				new Wait() { secs = 1.5 },
-				new Say { who = johnsonType, loopTag = "fiddling" },
-			],
-		};
-		newHardcodedNodes[["ChoiceCardRewardOfYourColorChoice_{{CharacterType}}"]] = new()
-		{
-			oncePerRun = true,
-			bg = typeof(BGBootSequence).Name,
-			allPresent = [johnsonType],
-			lines = [
-				new Say { who = johnsonType, loopTag = "squint" },
-				new Say { who = "comp", loopTag = "neutral" },
-			],
-		};
-
-		saySwitchNodes[["GrandmaShop"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "neutral"
-		};
-		saySwitchNodes[["LoseCharacterCard"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "neutral"
-		};
-		saySwitchNodes[["CrystallizedFriendEvent"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "fiddling"
-		};
-		saySwitchNodes[["ShopKeepBattleInsult"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "fiddling"
-		};
-		saySwitchNodes[["DraculaTime"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "squint"
-		};
-		saySwitchNodes[["Soggins_Infinite"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "flashing"
-		};
-		saySwitchNodes[["Soggins_Missile_Shout_1"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "neutral"
-		};
-		saySwitchNodes[["SogginsEscapeIntent_1"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "neutral"
-		};
-		saySwitchNodes[["SogginsEscape_1"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "fiddling"
-		};
-		saySwitchNodes[["AbandonedShipyard_Repaired"]] = new()
-		{
-			who = johnsonType,
-			loopTag = "neutral"
-		};
-	}*/
 }
